42c556b504070d42f23e2faf5c8895306a2f78e2
EmmanueleLibraro <emmanuelelibraro95@gmail.com>
2021-04-19 17:52:16 +0200

    .

--- JS/script.js
+++ JS/script.js
@@ -101,13 +101,21 @@ const icons = [
     },
 ];
 
+const colors = [
+    '#001858',
+    '#8bd3dd',
+    '#f582ae',
+];
 
 //CONTAINER ICON
 const container = document.querySelector('.icon');  //CHIAMIAMO IL DIV CHE AVVOLGE CON CLASS 'ICON'
 
 
 //1. STAMPARE LE ICONE A SCHERMO
-printIcons(icons, container);    //I DUE PARAMETRI CHE PORTEREMO GIU NELLE FUNZIONI
+//printIcons(icons, container);    //I DUE PARAMETRI CHE PORTEREMO GIU NELLE FUNZIONI
+
+//2. STAMPARE ICON
+const iconeColor = colorIcons(icons, colors);
 
 
 
@@ -124,7 +132,44 @@ printIcons(icons, container);    //I DUE PARAMETRI CHE PORTEREMO GIU NELLE FUNZI
 function printIcons(icons, container){
      //GENERARE MARKUP ICONE
     let html = '';               //VAR VUOTA PER GENERARE MARKUP
-    icons.forEach( (icons) => {
+    icons.forEach( (icon) => {
+        const {family, prefix, name} = icon;    //DESTRUTTURIAMO PER PRENDERE GLI OGGETTI INDICATI CHE POI VERRANNO INSERITI NELL'HTML PER RENDERLO DINAMICO
+        html +=                                             //COPIAMO PARTE DELL'HTML PER RENDERLO DINAMICO CON JS/                                                    
+        `<div class="icon p-20">
+            <i class="${family} ${prefix}${name}" style="color: #333"></i>       
+            <div class="title">${name}</div>
+        </div>`;
         
+    });//console.log(html);
+
+    //AGGIUNTA ICONE AL CONTAINER
+    container.innerHTML = html;
+};
+
+
+//COLLEZIONE ICONE COLORATE
+function colorIcons (icons, colors){
+    const types = getType(icons);        //CHIAMIAMO LA FUNZIONE SOTTO PER ITERARCI SOPRA 
+
+    //ASSEGNARE COLORE PER TIPO
+    const colorIcons = icons.map( (icon) =>{
+        const indexTyper = types.indexOf(icon.type);
+
+        return{
+            ...icon                                 //USIAMO LO SPREAD '...' PER COPIARE L'ARRAY COSI DA PERMMETTERCI DI COLORARE LE ICONE
+            
+        }
     });
+    return colorIcons
+};
+
+//PREPARAZIONE ICONE COLORATE
+function getType(icons){
+    const types = [];
+    icons.forEach( (icon) =>{
+        if(! types.includes(icon.type) ){          //SE NON C'E' TYPE LO PUSHA NELL'ARRAU TYPES
+            types.push(icon.type);
+        };
+    });
+    return types;
 };
